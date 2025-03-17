ee54731ceb06f2afca119852b11b8332ae6daa2d
Juan Pablo Méndez Nogales <jpmendezn@gmail.com>
2025-03-17 12:59:57 +0100

    Installed black as a linter, refactored main game module into a class, change manual call of ollama to use ollama-python, added new lighter models for execution, unlocked pygame thread when generating images and new scenarios, add loading screen

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 
 # SETTINGS
-OLLAMA_MODEL = "mistral" 
-STABLE_DIFFUSION_MODEL = "CompVis/stable-diffusion-v1-4"
+
+# OLLAMA_MODEL = "mistral"
+OLLAMA_MODEL = "mistral-nemo:12b-instruct-2407-q2_K"
+
+# STABLE_DIFFUSION_MODEL = "CompVis/stable-diffusion-v1-4"
+STABLE_DIFFUSION_MODEL = "nota-ai/bk-sdm-small"
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
--- game_logic/game_state.py
+++ game_logic/game_state.py
@@ -1,27 +1,35 @@
-'''
+"""
 Handle player stats and choices
-'''
+"""
+
 
 class GameState:
-    def __init__(self):
-        self.economy = 50
-        self.military = 50
-        self.public_appeal = 50
-        self.diplomacy = 50
-        self.turn = 1
+    def __init__(self, economy=50, military=50, public_appeal=50, diplomacy=50):
+        self.economy = economy
+        self.military = military
+        self.public_appeal = public_appeal
+        self.diplomacy = diplomacy
+        self.turn = 1  # Turn counter
         self.is_alive = True
 
-    def apply_choice(self, economy_change, military_change, public_change, diplomacy_change):
+    def apply_choice(self, chosen_option):
+
+        economy_change = chosen_option["effects"].get("economy", 0)
+        military_change = chosen_option["effects"].get("military", 0)
+        public_change = chosen_option["effects"].get("public_appeal", 0)
+        diplomacy_change = chosen_option["effects"].get("diplomacy", 0)
+
         self.economy += economy_change
         self.military += military_change
         self.public_appeal += public_change
         self.diplomacy += diplomacy_change
         self.turn += 1
 
-        #Game over condition
-        if self.economy <= 0 or self.military <= 0 or self.public_appeal <= 0 or self.diplomacy <= 0:
+        # Game over condition
+        if (
+            self.economy <= 0
+            or self.military <= 0
+            or self.public_appeal <= 0
+            or self.diplomacy <= 0
+        ):
             self.is_alive = False
-
-
-
-    
